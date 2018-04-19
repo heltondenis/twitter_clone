a1ce6caf176e983bfbb3b648c9dcdf167cfa0e1f
Helton Denis <heltondenis.sistemas@gmail.com>
2018-04-18 22:09:24 -0300

    verificação de usuário

--- controllers/loginController.php
+++ controllers/loginController.php
@@ -12,10 +12,30 @@ class loginController extends controller {
     }
 
     public function cadastro() {
-        $dados = array();
+        $dados = array('aviso'=>'');
 
         if (isset($_POST['nome']) && !empty($_POST['nome'])) {
-            
+
+            $nome = addslashes($_POST['nome']);
+            $email = addslashes($_POST['email']);
+            $senha = md5($_POST['senha']);
+
+            // Verifica se os dados realmente estão preenchidos 
+            if (!empty($nome) && !empty($email) && !empty($senha)) {
+
+                // Verifica se email existe
+                $u = new usuarios();
+
+                if (!$u->usuarioExiste($email)) {
+                     $_SESSION['twlg'] = $u->inserirUsuario($nome, $email, $senha);
+                     header("Location: /twitter");
+                } else {
+                    $dados['aviso'] = "Este usuário já existe!";
+                }
+ 
+                } else {
+                        $dados['aviso'] = "Preencha todos os campos";
+            }
         }
 
         $this->loadView('cadastro', $dados);
